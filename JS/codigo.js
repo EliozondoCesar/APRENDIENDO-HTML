8b8f24294164e56c3ed39603e1fd46f212640b38
CESAR ELIZONDO <gabrielelizondo5678@gmail.com>
2023-12-15 19:11:01 -0300

    fin

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,1 +1,7 @@
 console.log("Hola, aprendiendo a invocar código JS desde HTML")
+const parrafo = document.querySelector("p")
+const boton = document.querySelector("button")
+
+boton.addEventListener("click", ()=> {
+    console.log("Click");
+})
